d6838dc3d68e99097b0a0fbd1432edd4e6a4ea31
PedroMayD3v <137044417+PedroMayD3v@users.noreply.github.com>
2023-08-29 20:43:03 -0300

    FindById Implementation!

--- model/dao/impl/SellerDaoJDBC.java
+++ model/dao/impl/SellerDaoJDBC.java
@@ -1,37 +1,81 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
- 
-	
+
+	private Connection conn;
+
+	// Forçando a Injeção de dependencia
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+
+	}
 
 	@Override
 	public void insert(SellerDao obj) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(SellerDao obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName \r\n" + "FROM seller INNER JOIN department \r\n"
+							+ "ON seller.DepartmentId = department.Id \r\n" + "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			                               // Testando se minha consulta trouxe resultado e se sim o if faz passar para
+			                         // posição um se obtiver resultado
+			if (rs.next()) {
+				// Pegando o resultado e transformando em objetos
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+
+			}return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
+
 
 	@Override
 	public List<Seller> findAll(Integer id) {
@@ -39,6 +83,4 @@ public class SellerDaoJDBC implements SellerDao {
 		return null;
 	}
 
-	
-	
 }
