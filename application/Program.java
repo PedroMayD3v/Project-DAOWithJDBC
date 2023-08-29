80cd8e7603e79aaa1d363cb4c9db92fcede4dcb7
PedroMayD3v <137044417+PedroMayD3v@users.noreply.github.com>
2023-08-29 20:52:57 -0300

    First Test

--- application/Program.java
+++ application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
-import model.enities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
+		/*
+		 * Projeto DAO Com JDBC O padrão DAO (Data Access Object) é um padrão de projeto
+		 * de software Utilizado para separar a lógica de acesso a dados do restante da
+		 * aplicação.
+		 */
+
+		System.out.println("===== TESTE 1 =====");
+
+		// SellerDao pode instanciar daofactory por que o daofactory os implementa de
+		// certa forma
+		SellerDao sellerDao = DaoFactory.createSellerDao(); // Injeção de Dependecia
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
